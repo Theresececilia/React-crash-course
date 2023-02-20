ce8dfe0f87b378b63e6620fa1d51ffd3b1301bb1
Theresececilia <therese.johansson@edu.cmeducations.se>
2023-02-20 13:37:51 +0100

    add modal

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import classes from './Post.module.css'
 
-export default function Post(props) {
+export default function Post({ author, body}) {
 return (
     <li className={classes.post}>
-      <p className={classes.author} >{props.author}</p>
-      <p className={classes.text} >{props.body}</p>
+      <p className={classes.author} >{author}</p>
+      <p className={classes.text} >{body}</p>
     </li>
   )
 }
 
 
 //const names = ['Therese', 'Cecilia'];
-    //const chosenName = Math.random() > 0.5 ? names[0] : names[1];
+//const chosenName = Math.random() > 0.5 ? names[0] : names[1];
  
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,12 +1,45 @@
-
+import { useState } from 'react'
+import NewPost from './NewPost.jsx'
 import Post from './Post.jsx'
 import classes from './PostList.module.css'
+import Modal from './Modal.jsx'
 
 export default function PostList() {
+  const [ modalVisible, setModalVisible ] = useState(true) ;
+  const [ enteredBody, setEnteredBody ] = useState('');
+  const [ enteredAuthor, setEnteredAuthor ] = useState('');
+
+  function hideModalHandler() {
+    setModalVisible(false)
+  }
+
+  function bodyChangeHandler(e) {
+    setEnteredBody(e.target.value);
+  }
+
+  function authorChangeHandler(e) {
+    setEnteredAuthor(e.target.value);
+  }
+
+  let modalContent;
+  if (modalVisible) {
+    modalContent = (
+      <Modal onClose={hideModalHandler} >
+        <NewPost 
+          onBodyChange={bodyChangeHandler} 
+          onAuthorChange={authorChangeHandler} 
+        />
+      </Modal>
+    )
+  }
+
   return (
-    <ul className={classes.posts}>
-      <Post author="Therese" body="Im learning React.js" />
-      <Post author="Cecilia" body="This tutorial is Great!" />
-    </ul>
+    <>
+      {modalContent}
+      <ul className={classes.posts}>
+        <Post author={enteredAuthor} body={enteredBody} />
+        <Post author="Cecilia" body="This tutorial is Great!" />
+      </ul>
+    </>
   )
 }
